6d5a7bb0224ac66c6bb01f23efedf431f067e246
Ali <nazari.dev@gmail.com>
2019-05-08 17:44:38 +0430

    Update User model + Add PhoneNote model

---
+++ app/PhoneNote.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PhoneNote extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'phone_number', 'name', 'description',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\\User');
+    }
+}
