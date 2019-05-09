f6597c55d5137ac552fda9f4c0599695d7bc81b0
Ali <nazari.dev@gmail.com>
2019-05-09 18:57:56 +0430

    Create phone note Test

---
+++ tests/Feature/CreatePhoneNoteTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreatePhoneNoteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private $user;
+
+    /**
+     * Create a user for creating phone notes
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+    }
+
+    public function testSuccessCreate()
+    {
+        $this->actingAs($this->user);
+
+        $name = 'TestName';
+        $phoneNumber = '8877665';
+        $description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. \n Ab, accusamus accusantium commodi, consequatur debitis ducimus expedita iste laboriosam libero magnam natus nisi non optio quam quas quasi qui repellat velit?";
+        $response = $this->post('/phone-notes', [
+            'name' => $name,
+            'phone-number' => $phoneNumber,
+            'description' => $description,
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/phone-notes');
+        $this->assertDatabaseHas('phone_notes', [
+            'name' => $name,
+            'phone_number' => $phoneNumber,
+            'description' => $description
+        ]);
+    }
+
+    public function testInvalidPhoneNumber()
+    {
+        $this->get('/home');
+        $this->actingAs($this->user);
+
+        $response = $this->post('/phone-notes', [
+            'name' => 'Test',
+            'phone-number' => '(001) 44 55 667',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, accusamus accusantium commodi, consequatur debitis ducimus expedita iste laboriosam libero magnam natus nisi non optio quam quas quasi qui repellat velit?',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/home');
+    }
+
+    public function testEmptyDescription()
+    {
+        $this->get('/home');
+        $this->actingAs($this->user);
+
+        $response = $this->post('/phone-notes', [
+            'name' => 'Test',
+            'phone-number' => '8877665',
+            'description' => '',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/home');
+    }
+
+    public function testEmptyName()
+    {
+        $this->get('/home');
+        $this->actingAs($this->user);
+
+        $response = $this->post('/phone-notes', [
+            'name' => '',
+            'phone-number' => '8877665',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, accusamus accusantium commodi, consequatur debitis ducimus expedita iste laboriosam libero magnam natus nisi non optio quam quas quasi qui repellat velit?',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/home');
+    }
+
+    public function testEmptyPhoneNumber()
+    {
+        $this->get('/home');
+        $this->actingAs($this->user);
+
+        $response = $this->post('/phone-notes', [
+            'name' => 'Test',
+            'phone-number' => '',
+            'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, accusamus accusantium commodi, consequatur debitis ducimus expedita iste laboriosam libero magnam natus nisi non optio quam quas quasi qui repellat velit?',
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertRedirect('/home');
+    }
+}
